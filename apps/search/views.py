aa4ecd9fa08790c29b8e23b7f3cc1d6771777f7c
8208220723 <3236708415@qq.com>
2025-02-02 17:40:58 +0800

    更新了按内容（描述）搜索

--- apps/search/views.py
+++ apps/search/views.py
@@ -2,6 +2,8 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from apps.images.models import Image, ImageTag
 from datetime import datetime
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Create your views here.
 
@@ -9,7 +11,7 @@ from datetime import datetime
 class SelectImagesByTime(APIView):
     # 按时间搜索,格式：2025-08-20
     def post(self, request, *args, **kwargs):
-        # 识别搜素模式（按）
+        # 识别搜素模式（按时间搜索）
         userid = request.data.get("user_id")
         time = request.data.get("time")
         selectTime = datetime.strptime(time, "%Y-%m-%d").date()
@@ -90,3 +92,40 @@ class SelectImagesByTags(APIView):
                 "ids": idList,
             }
         )
+
+class SelectImagesByDescription(APIView):
+    # 按描述搜索
+    def post(self, request, *args, **kwargs):
+        userid = request.data.get("user_id")
+        description = request.data.get("description")
+
+        print("********************************************************")
+        print(request.data)
+        print("********************************************************")
+
+        images = Image.objects.filter(user_id=userid)
+
+        descriptionlist = [image.description for image in images]
+
+        # 使用TF-IDF对描述进行向量化
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform(descriptionlist + [description])
+
+        # 计算每个图片描述与输入描述的余弦相似度
+        similarity_scores = cosine_similarity(tfidf_matrix[-1], tfidf_matrix[:-1])
+
+        # 将相似度与图片关联
+        image_similarity_pairs = [(images[i], similarity_scores[0][i]) for i in range(len(images))]
+
+        # 过滤出余弦相似度大于0.1的图片
+        filtered_images = [pair for pair in image_similarity_pairs if pair[1] > 0.1]
+
+        # 按照相似度从高到低排序
+        filtered_images.sort(key=lambda x: x[1], reverse=True)
+
+        # 获取排序后的图片列表（返回url和id）
+        result = [{"image_id": image.id, "image_url": image.url} for image, _ in filtered_images]
+
+        return JsonResponse({"state": "success", "images": result})
+
+
--- pc_django/urls.py
+++ pc_django/urls.py
@@ -37,6 +37,7 @@ from apps.search.views import (
     SelectImagesByTime,
     SelectImagesByPosition,
     SelectImagesByTags,
+    SelectImagesByDescription,
 )
 
 urlpatterns = [
@@ -78,5 +79,6 @@ urlpatterns = [
         name="search_images_position",
     ),
     path("search/image/tags", SelectImagesByTags.as_view(), name="search_images_tags"),
+    path("search/image/description", SelectImagesByDescription.as_view(), name="search_images_description")
     # -----------------------------------------------------------------------------
 ]
